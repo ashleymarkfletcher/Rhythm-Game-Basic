b532f142d8fc681bca88b2a09e4aea1eee3c444e
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-10-14 16:27:39 +0100

    updated comments

--- sketch.js
+++ sketch.js
@@ -1,31 +1,35 @@
 
-var mySound
-var peakTimes = []
-var currentBeats = []
-var timeAhead = 3
-var timeBehind = 1
-var timelineHeight = 100
-var beatLine = 150
-var timelineBegin = 50
-var timelineEnd = 450
-var delay = 0.2
-var score = 0
-var timelineLength = timelineEnd - timelineBegin
+let mySound
+let peakTimes = []
+let currentBeats = []
+let score = 0
 
-var initThreshold = 0.8
-var minThreshold = 0.22
-var minPeaks = 300
+// define parameters for the timeline
+const timeAhead = 3
+const timeBehind = 1
+const timelineHeight = 100
+const beatLine = 150
+const timelineBegin = 50
+const timelineEnd = 450
+const delay = 0.2
+const timelineLength = timelineEnd - timelineBegin
 
+// define a beat
+const initThreshold = 0.8
+const minThreshold = 0.22
+const minPeaks = 300
 
+// load the song before starting
 function preload() {
-  // mySound = loadSound('DK.mp3');
-  mySound = loadSound('99.mp3');
-
+  mySound = loadSound('DK.mp3');
+  // mySound = loadSound('99.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   mySound.setVolume(1);
+
+  // find the beats in the song
   mySound.processPeaks(peaks, initThreshold, minThreshold, minPeaks)
 }
 
@@ -40,7 +44,7 @@ function draw() {
   // begin
   line(timelineBegin, timelineHeight-50, timelineBegin, timelineHeight+50)
 
-  var lineWant = map(beatLine, timelineBegin, timelineEnd, )
+  const lineWant = map(beatLine, timelineBegin, timelineEnd, )
   // beat
   line(beatLine, timelineHeight-50, beatLine, timelineHeight+50)
   // end
@@ -54,7 +58,6 @@ function draw() {
 function peaks(peaksArray) {
   console.log('peak!', peaksArray);
   peakTimes = peaksArray
-  console.log('peeee', peakTimes);
   mySound.play();
 }
 
@@ -63,7 +66,9 @@ function checkBeats() {
   var now = mySound.currentTime()
   // console.log('now = ', now);
   currentBeats = []
+
   // find if beats are after now
+  // for loop to break out early
   for (var i = 0; i < peakTimes.length; i++) {
     if (peakTimes[i] > now - timeBehind && peakTimes[i] <= now + timeAhead) {
       currentBeats.push(peakTimes[i])
@@ -77,11 +82,11 @@ function checkBeats() {
 
 function drawBeats() {
 
-  var now = mySound.currentTime()
+  const now = mySound.currentTime()
 
   currentBeats.forEach((beat) => {
     // scale 3 seconds to timeline
-    var x = map(beat, now - timeBehind, now + timeAhead, timelineBegin, timelineEnd)
+    const x = map(beat, now - timeBehind, now + timeAhead, timelineBegin, timelineEnd)
 
     ellipse(x ,timelineHeight, 20, 20)
     text(beat, x, timelineHeight)
@@ -90,18 +95,18 @@ function drawBeats() {
 
 function checkHit() {
 
-  var now = mySound.currentTime()
+  const now = mySound.currentTime()
 
+  // if a click is within a beat +1
   currentBeats.forEach((beat) => {
     if (now >= beat-delay && now <= beat+delay) {
       console.log('hit that shit!');
       score++
     }
-  });
+  })
 }
 
 function mousePressed() {
-  // console.log('press!');
   checkHit()
 }
 
